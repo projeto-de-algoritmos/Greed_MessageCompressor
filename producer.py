6ca5e58b5e2ef3be9db0bbc4e566245d77e6e991
zero101010 <igotaraujo97@gmail.com>
2022-07-25 01:23:42 -0300

    Update huffman structure to use an api to receive the request

--- producer.py
+++ producer.py
@@ -1,11 +1,11 @@
 import pika
 import json
 def sendMsgRabbitMq(queue,msg):
-    parameters = pika.URLParameters('STRING_DE_CONECAO')
+    parameters = pika.URLParameters('amqps://urjdtuuy:QQRrX2E2oquZA44mUB_gHm9m9HS270ME@beaver.rmq.cloudamqp.com/urjdtuuy')
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-    # channel.queue_declare(queue=queue')
     channel.basic_publish(exchange='',
                         routing_key=queue,
                         body=json.dumps(msg))
-    print(" [x] Sent 'Hello World!'")
+    print(" [x] Sent",json.dumps(msg))
+    return msg
