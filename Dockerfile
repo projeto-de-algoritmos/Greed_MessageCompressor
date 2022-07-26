7a1ee537a6e43fa1fb315f4281023e495bfa3036
zero101010 <igotaraujo97@gmail.com>
2022-07-25 22:49:53 -0300

    Add docker-compose and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,3 @@ FROM python:3.8-alpine
 WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
-ENTRYPOINT [ "python", "api.py" ]
--- decoder.py
+++ decoder.py
@@ -5,9 +5,7 @@ def decoder(binary_body):
     body = json.loads(binary_body.decode('utf-8'))
     message = body["msg"]
     body.pop('msg', None)
-    print(message)
     for key in body:
         body[key] = bitarray(body[key])
-    print(body)
     decode = bitarray(message).decode(body)
     print(''.join(decode))
